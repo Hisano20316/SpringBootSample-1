36bf2ccd8276e33f78977cd1cf84e726000aec23
yuya.hisano <volley0316hy@gmail.com>
2022-07-12 10:22:05 +0900

    バリデーション

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,18 +1,27 @@
 package com.example.controller;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
 
+import form.GroupOrder;
+import form.SignupForm;
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
     @Autowired
@@ -20,16 +29,25 @@ public class SignupController {
 
     /*ユーザ登録画面を表示*/
     @GetMapping("/signup")
-    public String getSignup(Model model) {
+    public String getSignup(Model model,Locale locale,@ModelAttribute SignupForm form) {
         //性別を取得
-        Map<String,Integer> genderMap = userApplicationService.getGenderMap();
+        Map<String,Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap",genderMap);
         return "user/signup";
     }
 
     /*ユーザ登録処理*/
     @PostMapping("/signup")
-    public String postSignup() {
+    public String postSignup(Model model,Locale locale,@ModelAttribute @Validated(GroupOrder.class)SignupForm form,BindingResult bindingResult) {
+
+        //入力チェック結果
+        if(bindingResult.hasErrors()) {
+            //NG:ユーザ登録画面に戻ります
+            return getSignup(model,locale,form);
+        }
+
+        log.info(form.toString());
+
         //ログイン画面へリダイレクト
         return "redirect:/login";
     }
